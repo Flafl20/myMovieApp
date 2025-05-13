2f72a8f13d79e27cbb3a3e8b55d2f94ad23b9a9d
Flafl20 <killertoo13@gmail.com>
2025-05-14 02:42:47 +0300

    just only the tranding or the fuatures

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,57 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Search from "./compunds/Search"
+import { Loading } from "./compunds/loading"
+import MovieCard from "./compunds/movieCard"
+import { useDebounce } from "react-use"
+
+const API_BASE_URL = "https://api.themoviedb.org/3"
+const API_KEY = import.meta.env.VITE_TMBD_API_KEY
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+}
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("")
+  const [errorMessage, setErrorMessage] = useState("")
+  const [movieList, setMovieList] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("")
+
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm])
+
+  const fetchMovies = async (query = "") => {
+    setIsLoading(true)
+    setErrorMessage("")
+
+    try {
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+      const response = await fetch(endpoint, API_OPTIONS)
+      if (!response.ok) {
+        throw new Error("failed to fetch movies")
+      }
+      const data = await response.json()
+      if (data.Response === "false") {
+        setErrorMessage(data.Error || "falid to fetch movies")
+        setMovieList([])
+        return
+      }
+      setMovieList(data.results)
+    } catch (err) {
+      console.error(`Error fetching movies: ${err}`)
+      setErrorMessage("Error fetching movies. please try again later")
+    } finally {
+      setIsLoading(false)
+    }
+  }
+  useEffect(() => {
+    fetchMovies(debouncedSearchTerm)
+  }, [debouncedSearchTerm])
   return (
     <main>
       <div className="pattern" />
@@ -14,8 +63,25 @@ function App() {
             Find <span className="text-gradient">Movies</span> You'll Enjoy
             Without the Hassle
           </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+
+        <section className="all-movies">
+          <h2 className="mt-[40px]">All Movies</h2>
+          {isLoading ? (
+            <Loading />
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </ul>
+          )}
+
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        </section>
       </div>
     </main>
   )
